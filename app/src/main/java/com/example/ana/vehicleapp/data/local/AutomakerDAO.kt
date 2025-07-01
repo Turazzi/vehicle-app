e9de827cbb3ad18a3f4c980bb7666fcf0ab2d9e7
Turazzi <analivia.turazzi@gmail.com>
2025-07-01 17:58:22 -0300

    feat: Adds DAOs interfaces

--- app/src/main/java/com/example/ana/vehicleapp/data/local/AutomakerDAO.kt
+++ app/src/main/java/com/example/ana/vehicleapp/data/local/AutomakerDAO.kt
@@ -1,4 +1,18 @@
 package com.example.ana.vehicleapp.data.local
 
-class AutomakerDAO {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.ana.vehicleapp.data.model.Automaker
+
+@Dao
+interface AutomakerDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(automakers: List<Automaker>)
+
+    @Query("SELECT * FROM automakers")
+    suspend fun getAll(): List<Automaker>
+
 }
--- app/src/main/java/com/example/ana/vehicleapp/data/local/VehicleDAO.kt
+++ app/src/main/java/com/example/ana/vehicleapp/data/local/VehicleDAO.kt
@@ -1,4 +1,25 @@
 package com.example.ana.vehicleapp.data.local
 
-class VehicleDAO {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.ana.vehicleapp.data.model.Vehicle
+
+
+@Dao
+interface VehicleDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(vehicles: List<Vehicle>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(vehicle: Vehicle)
+
+    @Query("SELECT * FROM vehicles")
+    suspend fun getAll(): List<Vehicle>
+
+    @Query("DELETE FROM vehicles WHERE id = :vehicleId")
+    suspend fun delete(vehicleId: Int)
+
 }
