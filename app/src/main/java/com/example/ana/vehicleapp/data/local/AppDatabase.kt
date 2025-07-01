d97721bd4df3999acefa0bd45373e03594e8df49
Turazzi <analivia.turazzi@gmail.com>
2025-07-01 17:58:48 -0300

    feat: Adds database abstract class

--- app/src/main/java/com/example/ana/vehicleapp/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/ana/vehicleapp/data/local/AppDatabase.kt
@@ -1,4 +1,14 @@
 package com.example.ana.vehicleapp.data.local
 
-class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.ana.vehicleapp.data.model.Automaker
+import com.example.ana.vehicleapp.data.model.Vehicle
+
+@Database(entities = [Automaker::class, Vehicle::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun automakerDao(): AutomakerDAO
+    abstract fun vehicleDao(): VehicleDAO
+
 }
