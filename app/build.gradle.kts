5c491b2badd7eb1ee8fbbc542fb8013954cb2de4
Turazzi <analivia.turazzi@gmail.com>
2025-07-02 18:00:44 -0300

    feat: implement MainViewModel to manage vehicle data and loading state

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,6 @@ dependencies {
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.3")
+
 }
--- app/src/main/java/com/example/ana/vehicleapp/ui/view/MainViewModel.kt
+++ app/src/main/java/com/example/ana/vehicleapp/ui/view/MainViewModel.kt
@@ -1,10 +1,48 @@
 package com.example.ana.vehicleapp.ui.view
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.ana.vehicleapp.data.model.Vehicle
 import com.example.ana.vehicleapp.data.repository.VehicleRepository
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
 
 class MainViewModel (private val repository: VehicleRepository) : ViewModel() {
 
+    private val _vehicles = MutableLiveData<List<Vehicle>>()
+    val vehicles: LiveData<List<Vehicle>> = _vehicles
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    init {
+        refreshVehicles()
+    }
+
+    private fun refreshVehicles() {
+        viewModelScope.launch {
+            _isLoading.value = true
+            try {
+                val vehicleList = repository.syncAndGetVehicles()
+                _vehicles.postValue(vehicleList)
+            }
+            catch (e:Exception) {
+                _vehicles.postValue(emptyList())
+            }
+            finally {
+                _isLoading.value = false
+            }
+        }
+    }
+
+    fun deleteVehicle(vehicle: Vehicle) {
+        viewModelScope.launch {
+            repository.deleteVehicle(vehicle.id)
+            refreshVehicles()
+        }
+    }
 
 
 }
